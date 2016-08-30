e1b5d3538fa3131ceef01388248a5ad6f1a2b847
wzb0180 <wzb0180@gmail.com>
2016-08-30 18:11:49 +0800

    add yum depends

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum -y install gcc gcc-c++ apr-devel apr-util-devel libtool autoconf automake libaio perl-devel perl-ExtUtils-Embed GeoIP GeoIP-devel make cmake ntpdate
 tar -zxvf openssl-1.0.2h.tar.gz
 cd openssl-1.0.2h
 ./config -fPIC --prefix=/usr/local/openssl enable-shared
