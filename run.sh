7c2c70a43546bf19c53ae08de7ed9535e71a0b51
wzb0180 <wzb0180@gmail.com>
2016-08-31 11:51:03 +0800

    run sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-basedir = /centos7-php7-resource
+basedir=/centos7-php7-resource
 
 yum -y install gcc gcc-c++ apr-devel apr-util-devel libtool autoconf automake libaio perl-devel perl-ExtUtils-Embed GeoIP GeoIP-devel make cmake ntpdate
 #openssl
